9506c61e3aa592a00a17830bc9a1607b4e5b823e
Amirdaly <amirdaly@yahoo.com>
2023-01-18 11:57:28 +0330

    added mux module and movie and director struct

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+)
 
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var Movies []Movie
+
+func main() {
+	r := mux.NewRouter()
 }
