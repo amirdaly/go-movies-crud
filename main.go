fcde10412d6a169c9734d2bf9c24cfe9d9ebf0ac
Amirdaly <amirdaly@yahoo.com>
2023-01-21 15:13:25 +0330

    fixed updateMovie function

--- main.go
+++ main.go
@@ -66,13 +66,15 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
 	for index, item := range movies {
-		movies = append(movies[:index], movies[index+1:]...)
-		var movie Movie
-		_ = json.NewDecoder(r.Body).Decode(&movie)
-		movie.ID = params["id"]
-		movies = append(movies, movie)
-		json.NewEncoder(w).Encode(movie)
-		return
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
 	}
 }
 
@@ -103,7 +105,7 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("UPDATE")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server on port 8000\n")
